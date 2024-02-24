8bbdca94fce1f5326d211a0df5fb86fb5a95bdf3
chaiyawat510-12 <139519167+chaiyawat510-12@users.noreply.github.com>
2024-02-24 18:42:06 +0700

    Add files via upload

---
+++ bi8.py
@@ -0,0 +1,36 @@
+n = 1
+while n <= 21:
+    print(n, end=', ')
+    n += 2
+
+print()  # เพื่อขึ้นบรรทัดใหม่
+
+
+n = 2
+while n <= 17:
+    print(n, end=', ')
+    n += 3
+
+print()
+
+
+n = 100
+while n >= -100:
+    print(n, end=', ')
+    n -= 10
+
+print()
+
+
+n = -30
+while n <= 30:
+    print(n, end=' , ')
+    n += 10
+
+print()
+
+
+n = 15
+while n <= 55:
+    print(n, end=', ')
+    n += 8
